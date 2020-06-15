8bd57ad5ce9c84855f3a362f4c1fed9e7a829c06
tonn24 <tonn.toomsalu@gmail.com>
2020-06-15 15:46:28 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BookShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BookShop'),
+      ),
+      body: Center(
+        child: Text(''),
+      ),
+    );
+  }
+}
